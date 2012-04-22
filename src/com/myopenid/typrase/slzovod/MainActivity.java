57c00c3ccc3dcac90c6ffdb2cc37e43aeadf1304
Jesse Glick <jglick@netbeans.org>
2012-04-22 11:22:31 -0400

    Initialized.

---
+++ src/com/myopenid/typrase/slzovod/MainActivity.java
@@ -0,0 +1,11 @@
+package com.myopenid.typrase.slzovod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
